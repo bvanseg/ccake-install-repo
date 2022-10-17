d5d9807f8a65763f82a90a256f83643a61e0a5db
bvanseg <bvanseg@gmail.com>
2022-10-17 10:48:00 -0400

    Updated gcc.ps1.

--- windows/gcc.ps1
+++ windows/gcc.ps1
@@ -1,4 +1,16 @@
+# Variables
 $downloadUrl = "https://github.com/brechtsanders/winlibs_mingw/releases/download/12.1.0-14.0.6-10.0.0-ucrt-r3/winlibs-x86_64-posix-seh-gcc-12.1.0-mingw-w64ucrt-10.0.0-r3.zip"
 $downloadPath = "$PSScriptRoot\winlibs-x86_64-posix-seh-gcc-12.1.0-mingw-w64ucrt-10.0.0-r3.zip"
+$ccakeCacheDir = "$HOME\.ccake\cache"
+$compilerDir = "$ccakeCacheDir\mingw64\bin"
+$exeDir = "$compilerDir\gcc.exe"
+
+# Download
 Invoke-WebRequest $downloadUrl -OutFile $downloadPath
-Expand-Archive $downloadPath -DestinationPath $PSScriptRoot -Force
+
+# Unzip
+New-Item -ItemType Directory -Force -Path $compilerDir
+Expand-Archive $downloadPath -DestinationPath $ccakeCacheDir -Force
+
+# Configure CCake
+& ccake configure --compiler-dir $exeDir
